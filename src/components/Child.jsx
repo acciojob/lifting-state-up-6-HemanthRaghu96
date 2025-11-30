ba1d621180e9ba112b1d0ff9005657ac5b78a590
HemanthRaghu96 <hemanthraghu96@gmail.com>
2025-11-30 21:32:46 +0530

    fix: updated

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
-const Child = ({ todos, handleComplete }) => {
+const TodoList = ({ todos, handleComplete }) => {
   return (
     <>
       <h1>Child Component</h1>
-      {todos &&
-        todos.length > 0 &&
-        todos.map((todo) => (
-          <ul key={todo.id}>
-            <li>
-              <p>{todo.text}</p>
 
-              {!todo.isCompleted && (
-                <button onClick={() => handleComplete(todo.id)}>
-                  Complete
-                </button>
-              )}
-            </li>
-          </ul>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <p>{todo.text}</p>
+
+            {!todo.isCompleted && (
+              <button onClick={() => handleComplete(todo.id)}>
+                Complete
+              </button>
+            )}
+          </li>
         ))}
+      </ul>
     </>
   );
 };
 
-export default Child;
+export default TodoList;
